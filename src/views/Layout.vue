e26b8ef04ceb87d43a86fe3546843061199d6c89
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-13 22:02:37 +0800

    头部分类导航交互

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -18,12 +18,18 @@
 import AppNavbar from '@/components/app-navbar.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '@/components/app-footer.vue'
+import { useStore } from 'vuex'
 export default {
   name: 'Layout',
   components: {
     AppNavbar,
     AppHeader,
     AppFooter
+  },
+  // 获取下分类数据
+  setup() {
+    const store = useStore()
+    store.dispatch('category/getList')
   }
 }
 </script>
