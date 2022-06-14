1e628d4ea0be0f4eebf2c17ce2ea296964bdc1f5
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-15 01:39:33 +0800

    完成首页主体-轮播图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="container">
-    <!-- 首页入口-->
-    <div class="home-entry">
+  <!-- 首页入口-->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
-import XtxSkeleton from '@/components/library/xtx-skeleton.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'HomePage',
   components: {
     HomeCategory,
-    XtxSkeleton
+    HomeBanner
   }
 }
 </script>
