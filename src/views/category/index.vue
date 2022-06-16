cb5f0cb79c1dc68e199b65f2396167505c16d8bd
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-16 16:25:25 +0800

    完成顶级类目-面包屑组件

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="top-category">TopCategory</div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <XtxBread>
+        <XtxBreadItem :to="{path:'/'}">首页</XtxBreadItem>
+        <XtxBreadItem>电器</XtxBreadItem>
+        <XtxBreadItem>空调</XtxBreadItem>
+      </XtxBread>
+      <!-- 轮播图 -->
+      <!-- 全部分类 -->
+      <!-- 各个分类推荐商品 -->
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'TopCategory'
+  name: 'TopCategory'
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped></style>
