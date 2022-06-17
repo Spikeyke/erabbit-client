c8d33478e068e34105089238394bcde8e1b0ca93
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-18 01:57:48 +0800

    完成二级类目-筛选区展示

--- src/api/category.js
+++ src/api/category.js
@@ -18,3 +18,11 @@ export const findAllCategory = () => {
 export const findTopCategory = (id) => {
     return request('/category', 'get', { id })
 }
+
+
+/* 
+    获取二级类目的筛选条件
+*/
+export const findSubCategoryFilter = (id) => {
+    return request('/category/sub/filter', 'get', { id })
+}
