499b07c00381aeb2e728ea452b74477c21b014de
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-18 10:20:43 +0800

    完成顶级类目复选框组件的v-model写法

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -4,16 +4,23 @@
       <!-- 面包屑 -->
       <SubBread />
       <SubFilter />
+      <XtxCheckbox v-model="isAllChecked"></XtxCheckbox>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
+import XtxCheckbox from '@/components/library/xtx-checkbox.vue'
 export default {
   name: 'SubCategory',
-  components: { SubBread, SubFilter }
+  components: { SubBread, SubFilter, XtxCheckbox },
+  setup() {
+    const isAllChecked = ref(true)
+    return { isAllChecked }
+  }
 }
 </script>
 
