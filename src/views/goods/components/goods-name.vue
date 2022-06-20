43aa4e5fb35259e7926c6da31acf0dce4834bc95
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-21 04:04:47 +0800

    完成-07商品详情-城市组件-交互逻辑

--- src/views/goods/components/goods-name.vue
+++ src/views/goods/components/goods-name.vue
@@ -12,7 +12,7 @@
     </dl>
     <dl>
       <dt>配送</dt>
-      <dd>至<XtxCity /> </dd>
+      <dd>至<XtxCity @change="changeCity" :fullLocation="fullLocation" /></dd>
     </dl>
     <dl>
       <dt>服务</dt>
@@ -27,16 +27,35 @@
 </template>
 
 <script>
-import XtxCity from '@/components/library/xtx-city.vue';
+import XtxCity from '@/components/library/xtx-city.vue'
+import { ref } from '@vue/reactivity'
 export default {
-    name: "GoodName",
-    props: {
-        goods: {
-            type: Object,
-            default: () => ({})
-        }
-    },
-    components: { XtxCity }
+  name: 'GoodName',
+  props: {
+    goods: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  setup(props) {
+    // 提供给后台的四项数据(没登陆)
+    const provinceCode = ref('110000')
+    const cityCode = ref('119900')
+    const countryCode = ref('110101')
+    const fullLocation = ref('北京市 市辖区 东城区')
+    // 取出用户收货地址中默认的地址给四个数据赋值（已登录）
+    if (props.goods.userAddresses) {
+      const defaultAddresses = props.goods.userAddresses.find(item => item.isDefault === 1)
+      if (defaultAddresses) {
+        provinceCode.value = defaultAddresses.provinceCode
+        cityCode.value = defaultAddresses.cityCode
+        countryCode.value = defaultAddresses.countryCode
+        fullLocation.value = defaultAddresses.fullLocationCode
+      }
+    }
+    return { fullLocation }
+  },
+  components: { XtxCity }
 }
 </script>
 
