fc4fa1189c1080898fbd59bd03cfb66e770b68fe
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-27 03:34:59 +0800

    完成-12-登录-QQ登录-回跳页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,9 @@ const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods/index')
+
 const Login = () => import('@/views/login/index')
+const LoginCallback = () => import('@/views/login/callback')
 
 // 路由规则
 const routes = [
@@ -20,7 +22,8 @@ const routes = [
       { path: '/product/:id', component: Goods }
     ]
   },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/login/callback', component: LoginCallback }
 ]
 
 // vue2.0 new VueRouter({}) 创建路由实例
