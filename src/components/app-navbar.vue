4acd60201bd73da9d41a7d96489c3a781032bc11
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-26 18:02:32 +0800

    完成-09-退出登录

--- src/components/app-navbar.vue
+++ src/components/app-navbar.vue
@@ -6,10 +6,12 @@
           <li>
             <a href="javascript:;"><i class="iconfont icon-user"></i>{{ profile.account }}</a>
           </li>
-          <li><a href="javascript:;">退出登录</a></li>
+          <li><a @click="logout()" href="javascript:;">退出登录</a></li>
         </template>
         <template v-else>
-          <li><RouterLink to="/login">请先登录</RouterLink></li>
+          <li>
+            <RouterLink to="/login">请先登录</RouterLink>
+          </li>
           <li><a href="javascript:;">免费注册</a></li>
         </template>
         <li><a href="javascript:;">我的订单</a></li>
@@ -27,6 +29,7 @@
 import { computed } from 'vue'
 // 使用vuex中的state需要设置计算属性
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 export default {
   name: 'AppTopnav',
   setup() {
@@ -35,33 +38,47 @@ export default {
     const profile = computed(() => {
       return store.state.user.profile
     })
-    return { profile }
+
+    //退出登录
+    // 1、清空本地存储信息和vuex的用户信息
+    // 2、跳转登录
+    const router = useRouter()
+    const logout = () => {
+      store.commit('user/setUser', {})
+      router.push('/login')
+    }
+    return { profile, logout }
   }
 }
 </script>
 <style scoped lang="less">
 .app-topnav {
   background: #333;
+
   ul {
     display: flex;
     height: 53px;
     justify-content: flex-end;
     align-items: center;
+
     li {
       a {
         padding: 0 15px;
         color: #cdcdcd;
         line-height: 1;
         display: inline-block;
+
         i {
           font-size: 14px;
           margin-right: 2px;
         }
+
         &:hover {
           color: @xtxColor;
         }
       }
-      ~ li {
+
+      ~li {
         a {
           border-left: 2px solid #666;
         }
@@ -69,5 +86,6 @@ export default {
     }
   }
 }
+
 // ~选择器作用：选择当前选择器后的所有元素
 </style>
