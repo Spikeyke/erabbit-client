557dbf351aee57e166fc8a7db8db00514b5cbef0
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-07-01 01:06:34 +0800

    完成-04-结算-收货地址-切换

--- src/views/member/pay/checkout.vue
+++ src/views/member/pay/checkout.vue
@@ -11,7 +11,7 @@
         <h3 class="box-title">收货地址</h3>
         <div class="box-body">
           <!-- 收货地址组件 -->
-          <CheckoutAddress :list="order.userAddresses" />
+          <CheckoutAddress @change="changeAddress" :list="order.userAddresses" />
         </div>
         <!-- 商品信息 -->
         <h3 class="box-title">商品信息</h3>
@@ -102,7 +102,14 @@ export default {
     createOrder().then(data => {
       order.value = data.result
     })
-    return { order }
+
+    // 提交订单：需要收货地址ID
+    const addressId = ref(null)
+    const changeAddress = id => {
+      addressId.value = id
+    }
+
+    return { order, changeAddress }
   }
 }
 </script>
