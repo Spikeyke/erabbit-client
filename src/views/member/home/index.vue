44653697aabb42a4273ba5fcbee836f0dd9d1a84
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-07-02 06:26:26 +0800

    02-个人中心-基础布局

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="member-home">MemberHome</div>
+  <div class="member-home">
+    <!-- 概览 -->
+    <HomeOverview1 />
+    <!-- 收藏 -->
+    <HomePanel title="我的收藏"></HomePanel>
+    <!-- 足迹 -->
+    <HomePanel title="我的足迹"></HomePanel>
+    <!-- 猜你 -->
+    <GoodsRelevant />
+  </div>
 </template>
 
 <script>
+import HomePanel from './components/home-panel.vue'
+import HomeOverview1 from './components/home-overview.vue'
+import GoodsRelevant from '@/views/goods/components/goods-relevant.vue'
 export default {
-  name: 'MemberHome'
+  name: 'MemberHome',
+  components: {
+    HomePanel,
+    HomeOverview1,
+    GoodsRelevant
+  }
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+:deep(.xtx-carousel) .carousel-btn.prev {
+  left: 5px;
+}
+:deep(.xtx-carousel) .carousel-btn.next {
+  right: 5px;
+}
+</style>
