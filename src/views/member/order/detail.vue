422fcd23178638b77117f917dea0aa9ea0f09d86
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-07-05 05:39:39 +0800

    15-订单详情-头部展示

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="member-order">MemberDetail</div>
+  <div class="member-order-detail" v-if="order">
+    <!-- 头部 -->
+    <DetailAction :order="order" />
+    <!-- 进度 -->
+    <!-- 物流 -->
+    <!-- 信息 -->
+  </div>
 </template>
 
 <script>
-export default {}
+import { ref } from '@vue/reactivity'
+import DetailAction from './components/detail-action.vue'
+import { findOrderDetail } from '@/api/order'
+import { useRoute } from 'vue-router'
+export default {
+  name: 'MemberDetail',
+  components: { DetailAction },
+  setup() {
+    const route = useRoute()
+    const order = ref(null)
+    findOrderDetail(route.params.id).then(data => {
+      order.value = data.result
+    })
+    return { order }
+  }
+}
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.member-order-detail {
+  background-color: #fff;
+  height: 100%;
+}
+</style>
